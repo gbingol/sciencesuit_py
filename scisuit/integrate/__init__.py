57a23c37727d1f3bdc5f1a3b2b845afa7ff996d6
Gökhan Bingöl <gbingol@hotmail.com>
2021-10-22 20:34:43 +0200

    added trapz method

--- scisuit/integrate/__init__.py
+++ scisuit/integrate/__init__.py
@@ -43,6 +43,36 @@ def cumtrapz(y, x=None, a=None, b=None, nodes=None, inter=10):
 
 
 
+def trapz(y, x=None, a=None, b=None, inter=100):
+      """
+      integrate y(x) using the trapezoidal rule.
+      """
+      
+      #notice that Vector is callable but here we are specifically looking for function (not the general callables)
+      if(not isinstance(y, types.FunctionType)):
+            IsYOK = isinstance(y, list) or isinstance(y, Vector)
+            IsXOK = isinstance(x, list) or isinstance(x, Vector)
+
+
+            if(IsYOK and IsXOK):    
+                  return SCISUITSYSTEM.trapz(x,y)
+            else:
+                  raise TypeError("x/y must be of type list/Vector")
+            
+      
+      elif(isinstance(y, types.FunctionType)):
+                        
+            IsOK = isinstance(a,numbers.Number) and isinstance(b, numbers.Number) and isinstance(inter, int)
+
+            if(IsOK):
+                  return SCISUITSYSTEM.trapz(y, a, b, inter)
+
+      raise TypeError("Unexpected combination of types.\n"
+                  "(y:callable, a:number, b:number, inter:int) \n"
+                  "(x:list/Vector, y:list/Vector)") 
+
+
+
 
 def simpson(y:Callable, a:float, b:float, inter=100)->float:
       """
