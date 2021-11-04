fde4f79442d789fadee538e01f22dc1cb1757786
Gökhan Bingöl <gbingol@hotmail.com>
2021-11-04 13:18:45 +0100

    f dist completed

--- scisuit/stats/__init__.py
+++ scisuit/stats/__init__.py
@@ -1,8 +1,22 @@
 from __SCISUIT import STATS as stat
 
+#binomial
 dbinom = stat.dbinom
 pbinom = stat.pbinom
 qbinom = stat.qbinom
 rbinom = stat.rbinom
+
+#chisq
+dchisq = stat.dchisq
+pchisq = stat.pchisq
+qchisq = stat.qchisq
+rchisq = stat.rchisq
+
+#F-dist
+df = stat.df
+pf = stat.pf
+qf = stat.qf
+rf = stat.rf
+
 
 from .mean import mean
