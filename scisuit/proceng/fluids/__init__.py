321a0297118a4414dca58f38b0c216e2590742f6
Gökhan Bingöl <gbingol@hotmail.com>
2021-12-18 14:45:31 +0100

    superheated added

--- scisuit/proceng/fluids/__init__.py
+++ scisuit/proceng/fluids/__init__.py
@@ -2,5 +2,6 @@ from .fluid import Fluid
 
 from .refrigerant import Refrigerant
 from .refrigerant import SaturatedRefrigerant
+from .refrigerant import SuperHeatedRefrigerant
 
 from .thermophysical import ThermoPhysical 
