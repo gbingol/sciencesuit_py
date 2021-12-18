f877b8cf02e7fc04dd03bf6da9e8c68f5b73e743
Gökhan Bingöl <gbingol@hotmail.com>
2021-12-18 14:45:06 +0100

    concept of path introduced using os.path

--- scisuit/gui/makeicon.py
+++ scisuit/gui/makeicon.py
@@ -1,18 +1,33 @@
+import os
 import wx
 
+from scisuit.gui import exepath
+
 def makeicon(path):
-      """
-      given an image full path, returns an icon
-      """
-      if(isinstance(path, str) == False):
-            raise TypeError("path must be of type string")
-      
-      icon = wx.Icon()
-      image = wx.Image()
-      image.LoadFile(path)
-      bmp=image.ConvertToBitmap()
-      icon.CopyFromBitmap(bmp)
-      
-      return icon
+	"""
+	path: image's relative (relative to ScienceSuit's exepath) or full path 
+	
+	returns an icon
+	"""
+	if(isinstance(path, str) == False):
+		raise TypeError("path must be of type string")
+	
+	FullPath = None
+
+	if(os.path.relpath(path)):
+		FullPath = exepath() + path
+	else:
+		FullPath = path
+	
+	if(os.path.exists(FullPath) == False):
+		raise ValueError("Invalid path:" + path)
+
+	icon = wx.Icon()
+	image = wx.Image()
+	image.LoadFile(FullPath)
+	bmp=image.ConvertToBitmap()
+	icon.CopyFromBitmap(bmp)
+
+	return icon
 
 
