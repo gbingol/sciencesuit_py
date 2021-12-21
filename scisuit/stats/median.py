27712736c6d957b77ea4dfafd8038f9b6f3011d8
Gökhan Bingöl <gbingol@hotmail.com>
2021-12-21 15:30:58 +0100

    added try except blocks

--- scisuit/stats/median.py
+++ scisuit/stats/median.py
@@ -1,4 +1,11 @@
 from scisuit.stats import quantile
 
-def median(x, sorted=False):
-      return quantile(x, 0.5, sorted)
+def median(x, Sorted=False):
+	retVal = None
+	try:
+		retVal = quantile(x, 0.5, Sorted)
+	except Exception as e:
+		print(e)
+		raise TypeError(e) from None
+	
+	return retVal
