51a597a1a046b039b3d3378fc1416673c78370b5
Gökhan Bingöl <gbingol@hotmail.com>
2021-12-21 15:32:14 +0100

    changed indentation to tabs

--- scisuit/stats/var.py
+++ scisuit/stats/var.py
@@ -3,27 +3,27 @@ from scisuit.core import Vector, Matrix, sqrt
 import math
 
 def var(y, ddof=0, axis=None):
-    TypeOK=isinstance(y, list) or isinstance(y, Vector) or isinstance(y, Matrix)
-    if(TypeOK == False):
-        raise TypeError("list / vector / Matrix expected")
+	TypeOK=isinstance(y, list) or isinstance(y, Vector) or isinstance(y, Matrix)
+	if(TypeOK == False):
+		raise TypeError("list / vector / Matrix expected")
     
-    AxisValOK = axis==None or axis==0 or axis==1
-    if(AxisValOK == False):
-        raise ValueError("axis must be 0 or 1")
+	AxisValOK = (axis== None or axis==0 or axis==1)
+	if(AxisValOK == False):
+		raise ValueError("axis must be 0 or 1")
     
-    if(isinstance(y, Matrix)):
-        return y.var(axis=axis, ddof=ddof)
+	if(isinstance(y, Matrix)):
+		return y.var(axis=axis, ddof=ddof)
     
-    if(isinstance(y, Vector)):
-        return y.var(ddof)
+	if(isinstance(y, Vector)):
+		return y.var(ddof)
     
-    v=Vector(y)
-    return v.var(ddof)
+	v=Vector(y)
+	return v.var(ddof)
 
 
 
 def stdev(y, ddof=0, axis=None):
-    if(isinstance(y, Matrix)):
-        return sqrt(y.var(axis=axis, ddof=ddof))
+	if(isinstance(y, Matrix)):
+		return sqrt(y.var(axis=axis, ddof=ddof))
     
-    return math.sqrt(var(y, ddof=ddof, axis=axis))
+	return math.sqrt(var(y, ddof=ddof, axis=axis))
