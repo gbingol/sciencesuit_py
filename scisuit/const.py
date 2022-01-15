78759d219ecc7e6de35c6f25c891a7af31b7aecf
Gökhan Bingöl <gbingol@hotmail.com>
2022-01-15 21:50:46 +0100

    new constants module

---
+++ scisuit/const.py
@@ -0,0 +1,61 @@
+fontstyle={
+'italic': "italic", 
+'normal': "normal", 
+'bold': "bold", 
+'single': "single", #single underline
+'none': "none" #no underline
+}
+
+fillstyle={
+'solid': 100, 
+'transparent': 106, 
+'bdiaghatch': 111, 
+'crossdiaghatch': 112, 
+'fdiaghatch': 113, 
+'crosshatch': 114, 
+'horizhatch': 115, 
+'verthatch': 116}
+
+linestyle={'solid': 100, 
+'dot': 101, 
+'longdash': 102, 
+'shortdash': 103, 
+'dotdash': 104, 
+'transparent': 106}
+
+
+colors={
+'aqua':"0 255 255", 
+'blue':"0 0 255",
+'blue_medium':"0 0 205", 
+'blue_royal':"65 105 225", 
+'blue_midnight':"25 25 112",
+'brown':"165 42 42", 
+'brown_saddle':"139 69 19",
+'chocolate':"210 105 30",  
+'crimson':"220 20 60",
+'fuchsia':"255 0 255",
+'gray':"128 128 128",
+'white':"255 255 255", 
+'red':"255 0 0", 
+'red_dark':"139 0 0", 
+'lime':"0 255 0", 
+'yellow':"255 255 0",
+'silver':"192 192 192", 
+'maroon':"128 0 0",
+'olive':"128 128 0", 
+'green':"0 128 0", 
+'purple':"128 0 128", 
+'teal':"0 128 128", 
+'navy':"0 0 128",
+'maroon':"128 0 0",  
+'salmon_dark':"233 150 122",
+'salmon':"250 128 114" , 
+'salmon_light':"255 160 122", 
+'orange_red':"255 69 0", 
+'orange_dark':"255 140 0", 
+'orange':"255 165 0",
+'tan':"210 180 140", 
+'wheat':"245 222 179", 
+'orchid':"218 112 214",
+'indigo':"75 0 130"}
