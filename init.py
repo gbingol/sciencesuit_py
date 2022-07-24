988d2ac551fb29c4aa464f3b0c757c13892ce6de
Gökhan Bingöl <gbingol@hotmail.com>
2022-07-24 15:52:56 +0200

    wx imported and run only if exists in pkg list

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 """
-It is recommended that you do NOT change or edit this file
+It is recommended NOT to change or to edit this file
 
 This file is run when the system starts, so anything wrong here will 
 either prevent system from starting or will break things
@@ -7,7 +7,6 @@ either prevent system from starting or will break things
 
 
 import sys
-import wx
 
 #access to scisuit package (or any other folder inside installation folder designated as package)
 sys.path.append('./')
@@ -22,5 +21,16 @@ since there can only be one main loop in wxPython in a single process.
 Attempting to use app.MainLoop elsewhere will crash the whole system 
 unless a subprocess is used.
 """
-app=wx.App(useBestVisual=True)
-app.MainLoop() #there can only be one main loop in wxPython
+
+import pkgutil
+
+wxInstalled = False
+for pkg in pkgutil.iter_modules():
+	if pkg.name == "wx":
+		wxInstalled = True
+		break
+		
+if(wxInstalled):
+	import wx
+	app=wx.App(useBestVisual=True)
+	app.MainLoop() #there can only be one main loop in wxPython
